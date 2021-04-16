5136b0fdf3986cd5f31178094fc0fe41f8c8f31a
Muhammad Umair Ghaznavi <upatni2@gmail.com>
2021-04-16 10:15:04 +0500

    update

--- resources/views/users/table.blade.php
+++ resources/views/users/table.blade.php
@@ -22,10 +22,20 @@
                         @endif
                     </td>
                     <td>
-                        <a class="btn btn-info" href="{{ route('users.show', $user->id) }}">Show</a>
-                        <a class="btn btn-primary" href="{{ route('users.edit', $user->id) }}">Edit</a>
-                        {!! Form::open(['method' => 'DELETE', 'route' => ['users.destroy', $user->id], 'style' => 'display:inline']) !!}
-                        {!! Form::submit('Delete', ['class' => 'btn btn-danger']) !!}
+                        {!! Form::open(['route' => ['users.destroy', $user->id], 'method' => 'delete']) !!}
+                        <div class='btn-group'>
+                            <a href="{{ route('users.show', [$user->id]) }}" class='btn btn-ghost-success'><i
+                                    class="fa fa-eye"></i></a>
+                            @can('user-edit')
+                                <a href="{{ route('users.edit', [$user->id]) }}" class='btn btn-ghost-info'><i
+                                        class="fa fa-edit"></i></a>
+                            @endcan
+                            @csrf
+                            @method('DELETE')
+                            @can('user-delete')
+                                {!! Form::button('<i class="fa fa-trash"></i>', ['type' => 'submit', 'class' => 'btn btn-ghost-danger', 'onclick' => "return confirm('Are you sure?')"]) !!}
+                            @endcan
+                        </div>
                         {!! Form::close() !!}
                     </td>
                 </tr>
